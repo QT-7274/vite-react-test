6055ff9402882c015fe7a7464634d7ad95de65d1
QT-7274 <2531376773@qq.com>
2025-08-01 12:23:27 +0800

    fix: 修改云函数

--- edge-functions/hello/index.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  async onRequest(context) {
-    return new Response('Hello from Edge Function!', {
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-    });
-  },
-};
